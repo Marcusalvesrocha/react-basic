f323155a4364d89b8b0e477a2335eb7ff77ce4a4
Marcus Rocha <sif.marcusvinicius@gmail.com>
2022-12-19 12:13:32 -0300

    adding test for Button to onclick and disabled

--- src/componets/Button/Button.spec.jsx
+++ src/componets/Button/Button.spec.jsx
@@ -1,5 +1,6 @@
+import userEvent from "@testing-library/user-event";
 import { Button } from ".";
-const { render, screen } = require("@testing-library/react")
+const { render, screen, fireEvent } = require("@testing-library/react")
 
 
 describe('<Button />', () => {
@@ -11,5 +12,25 @@ describe('<Button />', () => {
     expect(button).toBeInTheDocument();
   })
 
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text="Load More" onClick={fn}/>)
+    const button = screen.getByRole('button', {name: /load more/i})
+    userEvent.click(button)
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
 
+  it('should be disabled when disabled is true', () => {
+    render(<Button text="Load More" disabled={true}/>)
+    const button = screen.getByRole('button', {name: /load more/i})
+
+    expect(button).toBeDisabled()
+  })
+
+  it('should be enabled when disabled is false', () => {
+    render(<Button text="Load More" disabled={false}/>)
+    const button = screen.getByRole('button', {name: /load more/i})
+
+    expect(button).not.toBeDisabled()
+  })
 })
